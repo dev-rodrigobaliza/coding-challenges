b4d6c1e1f70487f9562668d587e89a0b4d12b820
dev-rodrigobaliza <dev.rodrigobaliza@gmail.com>
2023-11-21 18:41:33 -0300

    good enough for the moment

--- 08-redis-server/cmd/client/main.go
+++ 08-redis-server/cmd/client/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/go-redis/redis"
+)
+
+const (
+	maxReqs = 1000
 )
 
 func main() {
 	client := redis.NewClient(&redis.Options{Addr: "localhost:6379"})
 
+	light(client)
+	stress(client)
+}
+
+func light(client *redis.Client) {
 	st := client.Ping()
 	if st.Err() != nil {
 		panic(st.Err())
@@ -54,4 +66,26 @@ func main() {
 	}
 
 	println(res)
+}
+
+func stress(client *redis.Client) {
+	start := time.Now()
+	for i := 1; i < maxReqs; i++ {
+		key := fmt.Sprintf("key-%d", i)
+		value := fmt.Sprintf("value-%d", i)
+		st := client.Set(key, value, 0)
+		if st.Err() != nil {
+			panic(st.Err())
+		}
+
+		res, err := st.Result()
+		if err != nil {
+			panic(err)
+		}
+		if res != "OK" {
+			panic("got wrong set response: " + res)
+		}
+	}
+
+	fmt.Printf("%d requests took %s", maxReqs, time.Since(start).String())
 }
